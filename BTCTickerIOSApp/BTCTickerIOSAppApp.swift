30f7d0c997d6f2f3d6dc2f98437d2897b937c4fc
mattpus <mattpus@protonmail.ch>
2025-10-26 15:09:10 +0100

    Initial Commit

---
+++ BTCTickerIOSApp/BTCTickerIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCTickerIOSAppApp.swift
+//  BTCTickerIOSApp
+//
+//  Created by Matt on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BTCTickerIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
